1623fb0d62e2896f4ce9d212102d285761cc9c0c
AlharthAlhajHussein <alharth.alhaj.hussein@gmail.com>
2026-03-10 17:38:33 +0300

    add config variables

---
+++ src/helpers/config.py
@@ -0,0 +1,49 @@
+from pydantic_settings import BaseSettings
+from typing import Optional
+import os
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+SRC_DIR = os.path.dirname(CURRENT_DIR)
+ENV_PATH = os.path.join(SRC_DIR, ".env")
+
+class Settings(BaseSettings):
+    """Settings for the application."""
+    
+    app_name: str = "RAG Engine API"
+    app_version: str = "1.0.0"
+    
+    allowed_types: dict[str, str] = {
+        "application/pdf": "pdf",
+        "text/plain": "txt",
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document": "docx"
+    }
+    
+    db_user: Optional[str] = None
+    db_password: Optional[str] = None
+    db_name: Optional[str] = None
+    db_host: Optional[str] = "localhost"
+    db_port: Optional[int] = 5433
+    
+    
+    # Redis settings
+    redis_password: Optional[str] = None
+    redis_host: str = "localhost"
+    redis_port: int = 6379
+    
+    gemini_api_key: Optional[str] = None
+    
+    embedding_model: str = "gemini-embedding-001"
+    embedding_dimension: int = 768
+
+    # GCP
+    gcp_project_id: str = "pdf-ocr-extractor-488523"
+    gcp_storage_bucket: str = "my-arabic-ocr-bucket-12345"
+    
+    class Config:
+        env_file = ENV_PATH
+        env_file_encoding = "utf-8"
+        extra = "ignore"
+        
+        
+
+settings = Settings()
